e85f4b254aa318783fef4a3c5e14a934c0100bbf
William <bzfishmaster94@gmail.com>
2024-07-11 02:38:45 -0400

    Added some stuff in routes

--- routes/notes.js
+++ routes/notes.js
@@ -3,3 +3,11 @@ const path = require('path');
 const router = express.Router();
 const fs = require('fs');
 
+router.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error trying to read notes file', err);
+            return res.status(500).send('Error trying to read notes file');
+        }
+    })
+})
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/notes.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded( {extended: true }));
 
 app.use(express.static('public'));
+
+app.use('/api', api);
 
 app.get('*', (res, req) => {
     console.log('stuff from public/index.html file');
