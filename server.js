4722df5fe2a16e8a60a741bdeb1990b7bd05fbaa
William <bzfishmaster94@gmail.com>
2024-07-11 02:04:31 -0400

    Created some default server stuff

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded( {extended: true }));
+
+app.use(express.static('public'));
+
+
+app.get('/notes', (res, req) => {
+    console.log('Notes from public/notes.html receieved');
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`)
+})
