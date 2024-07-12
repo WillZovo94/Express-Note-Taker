321d6715c943ee1eea134fa0d018fa31e6a7d1bc
William <bzfishmaster94@gmail.com>
2024-07-12 19:24:45 -0400

    Added comments to code

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,15 @@
+// Grabs express
 const express = require('express');
+// Grabs path
 const path = require('path');
+// Crates a router using express
 const router = express.Router();
+// Grabs fs
 const fs = require('fs');
+// UUID is a dependency used for random generation of ID. 
 const { v4: uuidv4 } = require('uuid');
 
+// Grab the notes, check for an error. Then we JSON.parse the data.
 router.get('/notes', (req, res) => {
     fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
         if (err) {
@@ -21,6 +27,11 @@ router.get('/notes', (req, res) => {
     })
 })
 
+/* A post with /notes. Check if there's any inputs so an error can be thrown.
+    Then a file is read within the database and, if not, throws error. If it does go through,
+    we create a new object from the pushed content. Then write a file into the database with
+    the new object to store in the database.
+*/
 router.post('/notes', (req, res) => {
     console.log(req.body);
 
@@ -56,6 +67,8 @@ router.post('/notes', (req, res) => {
     })
 })
 
+
+// Deletes a note with the selected id.
 router.delete('/notes/:id', (req, res) => {
     const noteId = req.params.id;
 
@@ -67,9 +80,11 @@ router.delete('/notes/:id', (req, res) => {
         const notes = JSON.parse(data);
         const index = notes.findIndex(note => note.id === noteId);
         
+// Checks if index is not equal to -1, which shows that the potential deleted note has been found.
         if (index !== -1) {
+// We splice it so we can remove the element.
             notes.splice(index, 1);
-
+// Then write the updated notes.
             fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
                 if (err) {
                     console.log('Error trying to write notes file', err);
--- server.js
+++ server.js
@@ -1,22 +1,31 @@
+// defining express
 const express = require('express');
+// defining path
 const path = require('path');
+// defining api from the notes.js within the routes folder.
 const api = require('./routes/notes.js');
 
+// putting app as a variable that is considered express();
 const app = express();
+// Making port number, also process.env.PORT is useful for render.
 const PORT = process.env.PORT || 3001;
 
+// creating middleware below
 app.use(express.json());
 app.use(express.urlencoded( {extended: true }));
-
 app.use(express.static('public'));
-
+// to use middleware to a specific path
 app.use('/api', api);
 
+/* gets the '/notes' and console logs if recieved, and if it does, respond with
+    a send file of a joined path between __dirname and public/notes.html 
+*/
 app.get('/notes', (req, res) => {
     console.log('Notes from public/notes.html receieved');
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 })
 
+// just listens if the port ran through and if it does console.logs it.
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`)
 })
